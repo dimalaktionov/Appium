1f1de5fb101242d6981cc3fc5ea9f125cdcd936a
Dima <dima.laktionov5@gmail.com>
2019-02-18 16:27:37 +0200

    Made some changes

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,32 +1,27 @@
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
+import lib.ui.WelcomePageObject;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.net.URL;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
+    //private static final String PLATFORM_IOS = "ios";
+    //private static final String PLATFORM_ANDROID = "android";
 
 
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
 
     @Override
     protected void setUp() throws Exception
     {
         super.setUp();
-
-        DesiredCapabilities capabilities = this.getCapabilitiesByPlatformEnv();
-
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        driver = Platform.getInstance().getDriver();
         this.rotateScreenPortrait();
+        this.skipWelcomePageForIOSApp();
 
     }
 
@@ -42,7 +37,6 @@ public class CoreTestCase extends TestCase {
     {
         driver.rotate(ScreenOrientation.PORTRAIT);
     }
-
     protected void rotateScreenLandscape()
     {
         driver.rotate(ScreenOrientation.LANDSCAPE);
@@ -52,32 +46,11 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(seconds);
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception
+    private void skipWelcomePageForIOSApp()
     {
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if(platform.equals(PLATFORM_ANDROID)) {
-            //DesiredCapabilities capabilities = new DesiredCapabilities();
-
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName","AndroidtestDevice");
-            capabilities.setCapability("platformVersion","6.0");
-            capabilities.setCapability("automationName","Appium");
-            capabilities.setCapability("appPackage","org.wikipedia");
-            capabilities.setCapability("appActivity",".main.MainActivity");
-            capabilities.setCapability("app","/Users/macmini/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName","iPhone SE");
-            capabilities.setCapability("platformVersion","11.4");
-            capabilities.setCapability("app","/Users/macmini/Desktop/JavaAppiumAutomation/apks/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot get run platform from env variable. Platform value " + platform);
+        if(Platform.getInstance().isIOS()){
+            WelcomePageObject WelcomePageObject = new WelcomePageObject(driver);
+            WelcomePageObject.clickSkip();
         }
-
-        return capabilities;
-
-
     }
 }
---
+++ src/lib/ui/android/AndroidSearchPageObject.java
@@ -0,0 +1,21 @@
+package lib.ui.android;
+
+import io.appium.java_client.AppiumDriver;
+import lib.ui.SearchPageObject;
+
+public class AndroidSearchPageObject extends SearchPageObject
+{
+    static {
+        Search_init_element = "xpath://*[contains(@text, 'Search Wikipedia')]";
+                Search_input = "xpath://*[contains(@text, 'Search…')]";
+                SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn";
+                SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='{SUBSTRING}']";
+                SEARCH_RESULT_ELEMENT = "xpath://*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+                SEARCH_EMPTY_RESULT_ELEMENT = "//*[@text='No results found']";
+    }
+
+    public AndroidSearchPageObject(AppiumDriver driver)
+    {
+        super(driver);
+    }
+}
